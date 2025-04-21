88f994d07f13da2458853390f18bb395f26f0efa
Tan Chuen Keat <tanc0280@e.ntu.edu.sg>
2025-04-22 04:28:52 +0800

    Add Javadoc HTML files

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"controller"},{"l":"enums"},{"l":"helper"},{"l":"main"},{"l":"model"},{"l":"repository"},{"l":"view"}];updateSearchResults();
